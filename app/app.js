ce29a606cce31cea8e14669362411b7324896e29
Tara <tararee@gmail.com>
2016-07-30 22:07:03 -0400

    Finished modules

--- app/app.js
+++ app/app.js
@@ -1,3 +1,15 @@
 var myNinjaApp= angular.module("myNinjaApp",[]);
-//In order for the ng-app in index.html to know what to reference,
-//the ("myNinjaApp")has to match what is listed in ng-app=, not the var name.
+// All of the controllers, config, run methods will reside in this module!
+//This is how:
+myNinjaApp.config(){
+
+};
+//The config method is just a function that will fire BEFORE your application
+//runs.
+
+myNinjaApp.run(){
+
+};
+//The .run method fires AS your application runs
+
+myNinjaApp.controller
