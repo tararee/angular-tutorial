486a339c22d80d78370eedc325e88af5fd5076ef
Tara <tararee@gmail.com>
2016-07-30 21:57:56 -0400

    Explained how to connect the index.html to a module in app.js

--- app/app.js
+++ app/app.js
@@ -1,1 +1,3 @@
-var myNinjaApp= angular.module("myNinjaApp")
+var myNinjaApp= angular.module("myNinjaApp",[]);
+//In order for the ng-app in index.html to know what to reference,
+//the ("myNinjaApp")has to match what is listed in ng-app=, not the var name.
