07978e91f8708e167d9c4cf292a4e13f9be19070
Tara <tararee@gmail.com>
2016-07-31 16:48:23 -0400

    Added the app.js script source to the index.html

--- app/app.js
+++ app/app.js
@@ -1,15 +1,13 @@
 var myNinjaApp= angular.module("myNinjaApp",[]);
-// All of the controllers, config, run methods will reside in this module!
-//This is how:
+
 myNinjaApp.config(){
 
 };
-//The config method is just a function that will fire BEFORE your application
-//runs.
+
 
 myNinjaApp.run(){
 
 };
-//The .run method fires AS your application runs
+
 
 myNinjaApp.controller
