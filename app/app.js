621d3f7e437223a70383fbd4fdbe48119dd8339b
Tara <tararee@gmail.com>
2016-07-31 21:22:39 -0400

    Started Object-Oriented Programming and used .name to access the name of all objects

--- app/app.js
+++ app/app.js
@@ -1,14 +1,26 @@
 var myNinjaApp= angular.module("myNinjaApp",[]);
-//A controller controls our application data.
 
-// TO PROTECT SCOPE VARIABLES FROM MINIFICATION!!!
-//We have to pass the function and any dependencies through an array.
-//then we define the dependencies($scope here) within the arrays.
 myNinjaApp.controller('NinjaController', ['$scope', function($scope){
-// the scope of the .message is only within the div with ng-controller,
-// it wouldn't show up outside of is
-  $scope.message= "Hey y'all";
+// Object Oriented Programming!
+//Outputting whole objects in an array.
 
-  $scope.ninjas = ['yoshi', 'ryu', 'crystal',]
+  $scope.ninjas = [
+{
+  name: "Yoshi",
+  belt: "green"
+},
+{
+  name: "Crystal",
+  belt: "yellow"
+},
+{
+  name: "Ryu",
+  belt: "orange"
+},
+{
+  name: "Shaun",
+  belt: "black"
+}
+  ];
 
 }]);
