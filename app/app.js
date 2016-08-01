8a50939f159e0b9fd8a53b135609a96519901501
Tara <tararee@gmail.com>
2016-07-31 21:08:49 -0400

    Added the app.js script source to the index.html. Added dependencies/scope explanations to define a controller.

--- app/app.js
+++ app/app.js
@@ -1,11 +1,14 @@
 var myNinjaApp= angular.module("myNinjaApp",[]);
 //A controller controls our application data.
 
-myNinjaApp.controller('NinjaController', function($scope){
+// TO PROTECT SCOPE VARIABLES FROM MINIFICATION!!!
+//We have to pass the function and any dependencies through an array.
+//then we define the dependencies($scope here) within the arrays.
+myNinjaApp.controller('NinjaController', ['$scope', function($scope){
 // the scope of the .message is only within the div with ng-controller,
 // it wouldn't show up outside of is
   $scope.message= "Hey y'all";
 
   $scope.ninjas = ['yoshi', 'ryu', 'crystal',]
 
-});
+}]);
