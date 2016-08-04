7c4b92ed9b959e8bfb277305b3bd6fac6cfdf3f9
Tara <tararee@gmail.com>
2016-08-03 22:41:10 -0400

    Added ng-show

--- app/app.js
+++ app/app.js
@@ -8,22 +8,26 @@ myNinjaApp.controller('NinjaController', ['$scope', function($scope){
 {
   name: "Yoshi",
   belt: "green",
-  rate: 50
+  rate: 50,
+  available:true
 },
 {
   name: "Crystal",
   belt: "yellow",
-  rate: 30
+  rate: 30,
+  available:true
 },
 {
   name: "Ryu",
   belt: "orange",
-  rate: 10
+  rate: 10,
+  available:false
 },
 {
   name: "Shaun",
   belt: "black",
-  rate: 1000
+  rate: 1000,
+  available:true
 }
   ];
 
